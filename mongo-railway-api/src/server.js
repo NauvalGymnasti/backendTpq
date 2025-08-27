7dea869be1d461a3d77343d36bf0f84c1968beed
nauvalgymnasti7 <nauvalgymnasti7@gmail.com>
2025-08-27 11:42:24 +0700

    finally conected

--- mongo-railway-api/src/server.js
+++ mongo-railway-api/src/server.js
@@ -4,8 +4,8 @@ import cors from "cors";
 import morgan from "morgan";
 import { connectDB } from "./config/db.js";
 import users from "./routes/users.routes.js";
-import projects from "./routes/projects.routes.js";
-import tasks from "./routes/tasks.routes.js";
+// import students from "./routes/students.routes.js";
+// import teachers from "./routes/teachers.routes.js";
 
 const app = express();
 app.use(cors());
@@ -14,8 +14,8 @@ app.use(morgan("dev"));
 
 app.get("/health", (_req, res) => res.json({ ok:true, ts:new Date() }));
 app.use("/api/users", users);
-app.use("/api/projects", projects);
-app.use("/api/tasks", tasks);
+// app.use("/api/students", projects);
+// app.use("/api/teachers", tasks);
 
 const port = process.env.PORT || 4000;
 connectDB(process.env.MONGO_URI)
